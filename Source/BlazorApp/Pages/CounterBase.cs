324215bba014a5d142618093e14e451d2351c32e
JustinErdmier <justin.erdmier@outlook.com>
2022-06-30 23:05:04 -0500

    Add source code for Pages: Page Code Support.

---
+++ Source/BlazorApp/Pages/CounterBase.cs
@@ -0,0 +1,31 @@
+﻿//  ---------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Lieberman Technologies, LLC.  All rights reserved.
+//  Copyright file: CounterBase.cs  Copyright project: LearnBlazorServer
+//  ---------------------------------------------------------------------------------------------------------------------------------
+
+using BlazorApp.Data;
+
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Pages;
+
+public class CounterBase : ComponentBase
+{
+    [ Inject ]
+    public ICounterService CounterService { get; set; } = null!;
+
+    protected int CurrentCount;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        CurrentCount = CounterService.CurrentCount;
+    }
+
+    protected void IncrementCount()
+    {
+        CounterService.IncrementCount();
+        CurrentCount = CounterService.CurrentCount;
+    }
+}
---
+++ Source/BlazorApp/Pages/FetchData.cs
@@ -0,0 +1,23 @@
+﻿//  ---------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Lieberman Technologies, LLC.  All rights reserved.
+//  Copyright file: FetchData.cs  Copyright project: LearnBlazorServer
+//  ---------------------------------------------------------------------------------------------------------------------------------
+
+using BlazorApp.Data;
+
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Pages;
+
+public partial class FetchData
+{
+    [ Inject ]
+    public WeatherForecastService ForecastService { get; set; } = null!;
+
+    private WeatherForecast[]? forecasts;
+
+    protected override async Task OnInitializedAsync()
+    {
+        forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+    }
+}
