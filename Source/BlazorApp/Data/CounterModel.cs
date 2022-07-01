15ad29c18a03bfb87224c4f8b485ada4b9d13646
JustinErdmier <justin.erdmier@outlook.com>
2022-06-30 21:15:31 -0500

    Add Homework for section Included Systems.

---
+++ Source/BlazorApp/Data/CounterModel.cs
@@ -0,0 +1,20 @@
+﻿//  ---------------------------------------------------------------------------------------------------------------------------------
+//  Copyright (c) Lieberman Technologies, LLC.  All rights reserved.
+//  Copyright file: CounterModel.cs  Copyright project: LearnBlazorServer
+//  ---------------------------------------------------------------------------------------------------------------------------------
+
+namespace BlazorApp.Data;
+
+public class CounterModel
+{
+    // private readonly ILogger _logger;
+    //
+    // public CounterModel(ILogger logger) => _logger = logger;
+
+    public int CurrentCount { get; set; }
+
+    public void IncrementCount()
+    {
+        CurrentCount++;
+    }
+}
