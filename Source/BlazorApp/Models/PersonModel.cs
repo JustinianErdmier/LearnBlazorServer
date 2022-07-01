243bd6181a57c0818bbbf5a6aa1b148261a671f9
JustinErdmier <justin.erdmier@ltnow.com>
2022-07-01 13:30:02 -0500

    Add source code for Forms: Validation.

--- Source/BlazorApp/Models/PersonModel.cs
+++ Source/BlazorApp/Models/PersonModel.cs
@@ -1,10 +1,20 @@
-﻿namespace BlazorApp.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Models;
 
 public class PersonModel
 {
+    [ Required, StringLength(10, MinimumLength = 5, ErrorMessage = "Invalid first name length.") ]
     public string? FirstName { get; set; }
 
+    [ Required, StringLength(5, MinimumLength = 2, ErrorMessage = "Invalid first name length.") ]
     public string? LastName { get; set; }
+
+    [ Range(0, 125, ErrorMessage = "You need to enter a valid age.") ]
+    public int Age { get; set; }
+
+    [ Required, Range(typeof(bool), "true", "true", ErrorMessage = "You need to give consent to continue.") ]
+    public bool ConsentGiven { get; set; }
 
     public List<AddressModel>? Addresses { get; set; }
 }
