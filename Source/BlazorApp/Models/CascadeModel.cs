747bea47ed3061b49e3d6908410eb04d798c7473
JustinErdmier <justin.erdmier@ltnow.com>
2022-07-05 09:08:31 -0500

    Add source code for Forms: Advanced Form.

---
+++ Source/BlazorApp/Models/CascadeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp.Models;
+
+public class CascadeModel
+{
+    public int FirstId { get; set; }
+
+    public int SecondId { get; set; }
+}
---
+++ Source/BlazorApp/Models/SampleModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp.Models;
+
+public class SampleModel
+{
+    public string? SamplePassword { get; set; }
+
+    public int SampleInt { get; set; }
+
+    public double SampleDouble { get; set; }
+
+    public bool SampleBool { get; set; }
+
+    public DateTime SampleDateTime { get; set; }
+
+    public string? DropDownValue { get; set; }
+}
